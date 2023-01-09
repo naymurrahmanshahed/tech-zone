7aff517f176d5d5795fb843fdcc6bc311d1940ed
naymurrahmanshahed <shahed285@outlook.com>
2023-01-10 01:05:58 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer bg-violet-700 text-violet-50 text-center py-10">
+      &copy; {new Date().getFullYear()} Tech Zone. All Rights Reserved
+    </footer>
+  );
 };
 
 export default Footer;
